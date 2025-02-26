ebf198431d967c5fc3bef93e6bcfa04cb2df40da
Leo Huber <leo_huber@icloud.com>
2025-02-26 17:47:34 +0100

    Add code_cells.py for demonstration and implement data cleaning function in swiss_open_data.ipynb

---
+++ src/opendata/code_cells.py
@@ -0,0 +1,20 @@
+# %%
+msg = "Hello World"
+print(msg)
+nested_dict = {
+    'key1': {
+        'subkey1': 'value1',
+        'subkey2': 'value2'
+    },
+    'key2': {
+        'subkey3': 'value3',
+        'subkey4': 'value4'
+    }
+}
+print(nested_dict)
+
+
+# %%
+msg = "Hello again"
+print(msg)
+# %%
