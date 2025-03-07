2a391962be7933dc7013cfef59c3fe07ee95eb8a
Leo Huber <leo_huber@icloud.com>
2025-03-07 17:16:31 +0100

    Refactor main.py: improve code formatting, enhance readability, and add type hint to main function

--- src/llamaindextest/main.py
+++ src/llamaindextest/main.py
@@ -1,46 +1,47 @@
 import os
-import tiktoken
-from llama_index.llms.openai import OpenAI
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
-from llama_index.core.callbacks import CallbackManager, TokenCountingHandler
 import time
 
-from llama_index.core import Settings
+import tiktoken
+from llama_index.core import Settings, SimpleDirectoryReader, VectorStoreIndex
+from llama_index.core.callbacks import CallbackManager, TokenCountingHandler
+from llama_index.llms.openai import OpenAI
 
 token_counter = TokenCountingHandler(
-    tokenizer=tiktoken.encoding_for_model("gpt-3.5-turbo").encode
+    tokenizer=tiktoken.encoding_for_model("gpt-3.5-turbo").encode,
 )
 
 Settings.llm = OpenAI(model="gpt-3.5-turbo", temperature=0.2)
 Settings.callback_manager = CallbackManager([token_counter])
 
-def main():
-    openai_api_key = os.getenv('OPENAI_API_KEY')
+
+def main() -> None:
+    openai_api_key = os.getenv("OPENAI_API_KEY")
     if openai_api_key:
-        print(f'OPENAI_API_KEY: {openai_api_key}')
+        print(f"OPENAI_API_KEY: {openai_api_key}")
     else:
-        print('OPENAI_API_KEY is not set')
+        print("OPENAI_API_KEY is not set")
     start_time = time.time()
     documents = SimpleDirectoryReader("data").load_data()
     end_time = time.time()
     print(f"Time taken to load documents: {end_time - start_time} seconds")
-    
+
     start_index_time = time.time()
     index = VectorStoreIndex.from_documents(documents)
     end_index_time = time.time()
     print(f"Time taken to create index: {end_index_time - start_index_time} seconds")
-    
+
     retriever = index.as_retriever()
 
     query = ""
 
     nodes = retriever.retrieve(query)
     print(f"Number of nodes retrieved for query: {len(nodes)}")
-    
+
     query_engine = index.as_query_engine()
     response = query_engine.query(query)
     print(response)
     print(token_counter.prompt_llm_token_count)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
