cba20f2f76208437b9304ba1797978ca32d23e7c
Leo Huber <leo_huber@icloud.com>
2025-03-07 16:57:34 +0100

    Remove unused Jupyter notebook and clean up docstrings in ckanlib.py for consistency

--- src/opendata/ckanlib.py
+++ src/opendata/ckanlib.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """
 
 --> https://docs.ckan.org/en/latest/api/index.html#module-ckan.logic.action.get
@@ -17,8 +16,8 @@ Functions:
         Raises:
             ckanapi.errors.CKANAPIError: If an error occurs while fetching organizations from CKAN API.
 """
-import ckanapi
 
+import ckanapi
 
 
 class Config:
@@ -29,19 +28,20 @@ class Config:
     @property
     def base_url(self):
         return self._base_url
-    
+
     @property
     def client(self):
         return self._client
 
-def get_organisations(config: Config, search_string:str = "") -> list[str]:
-    '''Fetches and filters organizations from CKAN based on a search string.
+
+def get_organisations(config: Config, search_string: str = "") -> list[str]:
+    """Fetches and filters organizations from CKAN based on a search string.
     Args:
         config (Config): An instance of the Config class containing CKAN API client.
         search_string (str): The string to filter organizations by.
     Returns:
         list[str]: A list of organization names that match the search string.
-    '''
+    """
     try:
         # Call the organization_list action to get a list of all organizations.
         orgs: list[str] = config.client.action.organization_list()
@@ -50,15 +50,16 @@ def get_organisations(config: Config, search_string:str = "") -> list[str]:
     except ckanapi.errors.CKANAPIError as e:
         print("An error occurred while fetching organizations:", e)
         return []
-    
-def get_groups(config: Config, search_string:str = "") -> list[str]:
-    '''Fetches and filters groups from CKAN based on a search string.
+
+
+def get_groups(config: Config, search_string: str = "") -> list[str]:
+    """Fetches and filters groups from CKAN based on a search string.
     Args:
         config (Config): An instance of the Config class containing CKAN API client.
         search_string (str): The string to filter groups by.
     Returns:
         list[str]: A list of groups that match the search string.
-    '''
+    """
     try:
         # Call the group_list action to get a list of all groups.
         groups: list[str] = config.client.action.group_list()
@@ -67,15 +68,16 @@ def get_groups(config: Config, search_string:str = "") -> list[str]:
     except ckanapi.errors.CKANAPIError as e:
         print("An error occurred while fetching groups:", e)
         return []
-    
-def get_tags(config: Config, search_string:str = "") -> list[str]:
-    '''Fetches and filters tags from CKAN based on a search string.
+
+
+def get_tags(config: Config, search_string: str = "") -> list[str]:
+    """Fetches and filters tags from CKAN based on a search string.
     Args:
         config (Config): An instance of the Config class containing CKAN API client.
         search_string (str): The string to filter tags by.
     Returns:
         list[str]: A list of tags that match the search string.
-    '''
+    """
     try:
         # Call the tag_list action to get a list of all tags.
         tags: list[str] = config.client.action.tag_list()
@@ -84,15 +86,16 @@ def get_tags(config: Config, search_string:str = "") -> list[str]:
     except ckanapi.errors.CKANAPIError as e:
         print("An error occurred while fetching tags:", e)
         return []
-    
+
+
 def get_organization_details(config: Config, organization_id: str) -> dict:
-    '''Fetches details of a specific organization from CKAN based on the organization ID.
+    """Fetches details of a specific organization from CKAN based on the organization ID.
     Args:
         config (Config): An instance of the Config class containing CKAN API client.
         organization_id (str): The ID of the organization to fetch details for.
     Returns:
         dict: A dictionary containing details of the organization.
-    '''
+    """
     try:
         # Call the organization_show action to get details of a specific organization.
         org_details: dict = config.client.action.organization_show(id=organization_id)
@@ -100,33 +103,34 @@ def get_organization_details(config: Config, organization_id: str) -> dict:
     except ckanapi.errors.CKANAPIError as e:
         print("An error occurred while fetching organization details:", e)
         return {}
-    
-def package_search(config: Config, search_string:str = "", filter_criterias=[]) -> list[dict]:
-    '''Searches for packages on CKAN based on a search string.
+
+
+def package_search(config: Config, search_string: str = "", filter_criterias=[]) -> list[dict]:
+    """Searches for packages on CKAN based on a search string.
     Args:
         config (Config): An instance of the Config class containing CKAN API client.
         search_string (str): The string to search for in package names.
     Returns:
         list[dict]: A list of dictionaries containing details of packages that match the search string.
-    '''
+    """
     try:
         # Call the package_search action to search for packages based on a search string.
         search_results: dict = config.client.action.package_search(q=search_string, fq_list=filter_criterias)
-        packages: list[dict] = search_results.get('results', [])
+        packages: list[dict] = search_results.get("results", [])
         return packages
     except ckanapi.errors.CKANAPIError as e:
         print("An error occurred while searching for packages:", e)
         return []
-    
+
 
 def get_package_details(config: Config, package_id: str) -> dict:
-    '''Fetches details of a specific package or dataset from CKAN based on the package ID.
+    """Fetches details of a specific package or dataset from CKAN based on the package ID.
     Args:
         config (Config): An instance of the Config class containing CKAN API client.
         package_id (str): The ID of the package to fetch details for.
     Returns:
         dict: A dictionary containing details of the package.
-    '''
+    """
     try:
         # Call the package_show action to get details of a specific package.
         package_details: dict = config.client.action.package_show(id=package_id)
